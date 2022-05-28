78ebe4e063017b7d6a758494a555b30b309fc113
Eshaan Barkataki <eshaanbarkataki@gmail.com>
2022-05-28 15:15:39 -0400

    Rewrote website

--- main.js
+++ main.js
@@ -1,40 +1,21 @@
-globalIndex = 0
+function selectedTab (index) {
+    let tabs = [document.getElementById("AboutMeTab"), document.getElementById("MyProjectTab"), document.getElementById("ContactsTab")];
+    let texts = [document.getElementById("AboutMeText"), document.getElementById("MyProjectText"), document.getElementById("ContactsText")];
+    let tabLines = [document.getElementById("AboutMeTabLine"), document.getElementById("MyProjectTabLine"), document.getElementById("ContactsTabLine")]
+    let style = getComputedStyle(document.body); 
 
-window.onresize =  function() {
-    let firstTabWidth = document.getElementsByClassName("label")[0].offsetWidth;
-    let secondTabWidth = document.getElementsByClassName("label")[1].offsetWidth;
-    let thirdTabWidth = document.getElementsByClassName("label")[2].offsetWidth;      
-    let line = document.getElementById("hr")
-    if (globalIndex == 0) {
-        line.style.left = "0px";
-        line.style.width = firstTabWidth - 2 + "px";
-    }
-    else if (globalIndex == 1) {
-        line.style.left = firstTabWidth + "px";
-        line.style.width = secondTabWidth - 2 + "px";
-    }
-    else if (globalIndex == 2) {
-        line.style.left = firstTabWidth + secondTabWidth + "px";
-        line.style.width = thirdTabWidth - 2 + "px";
-    }
-}
-
-function tabSelect (index) {
-    globalIndex = index
-    let firstTabWidth = document.getElementsByClassName("label")[0].offsetWidth;
-    let secondTabWidth = document.getElementsByClassName("label")[1].offsetWidth;
-    let thirdTabWidth = document.getElementsByClassName("label")[2].offsetWidth;      
-    let line = document.getElementById("hr")
-    if (globalIndex == 0) {
-        line.style.left = "0px";
-        line.style.width = firstTabWidth - 2 + "px";
-    }
-    else if (globalIndex == 1) {
-        line.style.left = firstTabWidth + "px";
-        line.style.width = secondTabWidth - 2 + "px";
-    }
-    else if (globalIndex == 2) {
-        line.style.left = firstTabWidth + secondTabWidth + "px";
-        line.style.width = thirdTabWidth - 2 + "px";
+    console.log(tabs);
+    console.log(texts);
+    for (let i = 0; i < tabs.length; i++) {
+        if (i != index) {
+            tabs[i].style.backgroundColor = style.getPropertyValue("--tab_color");
+            texts[i].style.display = "none";
+            tabLines[i].style.display = "none";
+        }
+        else {
+            tabs[i].style.backgroundColor = style.getPropertyValue("--tab_selected_color");
+            texts[i].style.display = "block";
+            tabLines[i].style.display = "block";
+        }
     }
 }
